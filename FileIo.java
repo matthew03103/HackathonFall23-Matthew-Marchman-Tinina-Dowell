a298f8afb1c06101ed850488e104509f21d7740f
Matthew Marchman <116112455+matthew03103@users.noreply.github.com>
2023-09-14 19:11:20 -0400

    Add files via upload

---
+++ FileIo.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileIo {
+
+    public static void sort(){
+        try {
+            File file = new File();
+            Scanner scan = new Scanner(file);
+            while(scan.hasNextLine()){
+
+            }
+        }
+        catch (IOException){
+        }
+    }
+}
