5c44645023bbdf911c4e808819179770db0f29ae
Tinina Dowell <salaynedowell@gmail.com>
2023-09-15 12:18:11 -0400

    Update StorageUnit.java

--- StorageUnit.java
+++ StorageUnit.java
@@ -1,34 +1,11 @@
 public class StorageUnit extends Container{
-    StorageUnit() {
-        double length = 20;
-        double height = 8;
-        double width = 10;
 
-        setLength(length);
-        setHeight(height);
-        setWidth(width);
+    public StorageUnit() {
 
+        super.setLength(20);
+        super.setWidth(10);
+        super.setHeight(8);
 
     }
-     @Override
-    public void setLength(double length) {
-        super.setLength(length);
-    }
 
-    @Override
-    public void setHeight(double height) {
-        super.setHeight(height);
-    }
-
-    @Override
-    public void setWidth(double width) {
-        super.setWidth(width);
-    }
-
-
-
-    @Override
-    public double volume() {
-        return getLength() * getHeight() * getWidth();
-    }
 }
