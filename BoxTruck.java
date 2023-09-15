bfcf1adf1b71781b9f251ae5b4745dc743966e7b
Tinina Dowell <salaynedowell@gmail.com>
2023-09-14 20:46:43 -0400

    Update BoxTruck.java

--- BoxTruck.java
+++ BoxTruck.java
@@ -12,6 +12,25 @@ public class BoxTruck extends Container{
         setMaxWeight(maxWeight);
 
     }
+     @Override
+    public void setLength(double length) {
+        super.setLength(length);
+    }
+
+    @Override
+    public void setHeight(double height) {
+        super.setHeight(height);
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+    }
+
+    @Override
+    public void setMaxWeight(long maxWeight) {
+        super.setMaxWeight(maxWeight);
+    }
 
 
     @Override
