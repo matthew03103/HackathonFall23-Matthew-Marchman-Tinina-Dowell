d8dba4df82d40e69c9450089c48303274b584713
Tinina Dowell <salaynedowell@gmail.com>
2023-09-15 12:15:15 -0400

    Update ShippingContainer.java

--- ShippingContainer.java
+++ ShippingContainer.java
@@ -1,39 +1,12 @@
 public class ShippingContainer extends Container{
-    ShippingContainer() {
-        double length = 40;
-        double height = 8.5;
-        double width = 8;
-        long maxWeight = 59200;
-        setLength(length);
-        setHeight(height);
-        setWidth(width);
-        setMaxWeight(maxWeight);
 
-    }
-     @Override
-    public void setLength(double length) {
-        super.setLength(length);
-    }
+        // Constructor for ShippingContainer
+        public ShippingContainer() {
 
-    @Override
-    public void setHeight(double height) {
-        super.setHeight(height);
-    }
-
-    @Override
-    public void setWidth(double width) {
-        super.setWidth(width);
-    }
-
-    @Override
-    public void setMaxWeight(long maxWeight) {
-        super.setMaxWeight(maxWeight);
-    }
-
-
-    @Override
-    public double volume() {
-        return getLength() * getHeight() * getWidth();
-    }
+            super.setLength(40);
+            super.setWidth(8);
+            super.setHeight(8);
+            super.setWeight(59200);
+        }
 
 }
