d50e7ff65e206dfb38c12a2a5048bff35b1fce2c
Tinina Dowell <salaynedowell@gmail.com>
2023-09-14 20:46:59 -0400

    Update Pallet.java

--- Pallet.java
+++ Pallet.java
@@ -10,6 +10,25 @@ public class Pallet extends Container{
     setMaxWeight(maxWeight);
 
 }
+     @Override
+    public void setLength(double length) {
+        super.setLength(length);
+    }
+
+    @Override
+    public void setHeight(double height) {
+        super.setHeight(height);
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+    }
+
+    @Override
+    public void setMaxWeight(long maxWeight) {
+        super.setMaxWeight(maxWeight);
+    }
 
 
     @Override
