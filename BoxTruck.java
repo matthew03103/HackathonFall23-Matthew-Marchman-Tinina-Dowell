1d9e0a787615835db3479a02fb98bb2613b0a420
Tinina Dowell <salaynedowell@gmail.com>
2023-09-15 20:47:05 -0400

    Update BoxTruck.java

--- BoxTruck.java
+++ BoxTruck.java
@@ -1,4 +1,9 @@
 public class BoxTruck extends Container{
+
+    public static Pallet pallet = new Pallet();
+
+    //String [Width][Length]
+    public static String[][] boxTruck = new String[8][24];
     public BoxTruck() {
 
         super.setLength(24);
@@ -7,6 +12,30 @@ public class BoxTruck extends Container{
         super.setWeight(26000);
 
     }
+    public static void addPallet(String[][] truck){
+        int counter=0;
+        for (int i = 0; i < 8/ pallet.getWidth(); i++) {
+            for (int j = 0; j < 24/ pallet.getLength(); j++) {
+                counter++;
+                boxTruck[j][i] = "Pallet "+ counter;
+            }
+        }
+    }
+    public static void printPallet(String[][] truck) {
+        int counter = 0;
+        for (int i = 0; i < 24 / pallet.getLength(); i++) {
+                System.out.println();
 
+            for (int j = 0; j < 8 / pallet.getWidth(); j++) {
+                counter++;
+                System.out.println(boxTruck[i][j]);
+            }
 
+        }
+    }
+
+    public static void main(String[] args) {
+        addPallet(boxTruck);
+        printPallet(boxTruck);
+    }
 }
