5210bd6525662798fe8d79842574f989db7d055a
Tinina Dowell <salaynedowell@gmail.com>
2023-09-15 20:47:49 -0400

    Update ShippingContainer.java

--- ShippingContainer.java
+++ ShippingContainer.java
@@ -1,7 +1,7 @@
 public class ShippingContainer extends Container{
 
-
-
+    public static String [][][] container=new String[40][8][8];
+    public static Box box = new Box(1, 5, 2, 5);
 
         // Constructor for ShippingContainer
         public ShippingContainer() {
@@ -12,6 +12,8 @@ public class ShippingContainer extends Container{
             super.setWeight(59200);
         }
 
+    // these 3 for loops fill the array with boxes.
+
     public static void fillArray(String[][][] container, Box box) {
 
         int counter = 0;
@@ -20,21 +22,20 @@ public class ShippingContainer extends Container{
         boolean full = false;
 
 
-        // these 3 for loops fill the array with boxes.
+
         //length
 
         if (maxWeight > temp){
-            for (int i = 0; i < 48; i++) {
+            for (int i = 0; i < 40/box.getLength(); i++) {
                 //  System.out.println();
 
                 //height
-                for (int j = 0; j < 5; j++) {
+                for (int j = 0; j < 8/box.getHeight(); j++) {
                     //      System.out.println();
                     //width
-                    for (int k = 0; k < 40; k++) {
-
-                        container[i][j][k] = "box " + counter + " ";
+                    for (int k = 0; k < 8/box.getWidth(); k++) {
                         counter++;
+                        container[i][j][k] = "box " + counter + " ";
                     }
                 }
             }
@@ -46,19 +47,24 @@ public class ShippingContainer extends Container{
 
     public static void printArray(String[][][] container){
         //length
-        for (int i = 0; i < 48; i++){
+        for (int i = 0; i < 40/box.getLength(); i++){
             System.out.println();
 
             //height
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < 8/ box.getHeight(); j++) {
                 System.out.println();
                 //width
-                for (int k = 1; k < 40; k++) {
+                for (int k = 0; k < 8/ box.getWidth(); k++) {
                     System.out.print(container[i][j][k]);
 
                 }
             }
         }
+    }
+
+    public static void main(String[] args) {
+        fillArray(container, box);
+        printArray(container);
     }
 
 
